4cfc1baeb87c34ca8afda013fecd90b051634c0f
Henrique Dumay <henriquedumay@gmail.com>
2023-12-03 22:23:54 -0300

    .

--- aplicacao_tab.R
+++ aplicacao_tab.R
@@ -4,7 +4,7 @@ library(heemod)
 # Parâmetros
 # ------------------------------------------------------------------------
 par_mod <- define_parameters(
-  age_base    = 20,
+  age_base    = 25,
   age_cycle = model_time + age_base,
   p_death_all = 262828 / 214300000, # https://cieges.conass.org.br/paineis/listagem/situacao-de-saude-da-populacao/painel-de-analise-do-excesso-de-mortalidade-por-causas-naturais-no-brasil
   p_death_disease = 0.2       , # Probabilidade de morte por suicídio em TAB
@@ -13,30 +13,14 @@ par_mod <- define_parameters(
      p_death_disease),
   p_disease_base = 0.9       , # Probabilidade da doença na população geral
   med_effect = 0.5            , # Efeito da medicação
-  prevalencia = 0.05,
+  prevalencia = 0.04,
   p_disease_med = p_disease_base * med_effect , # Probabilidade recaída durante tratamento
-  cost_hospit_cycle = 100000  , # Custo de cada ciclo de hospitalização
+  cost_hospit_cycle = 33090  , # Custo de cada ciclo de hospitalização
   p_cured_base = 0.2,
   p_cured_med = 0.7,
-  cost_med = 5000,
+  cost_med = 1904,
   dr = 0.05,
   qaly_disease = 0.5)
-
-  # p_doença = 0.03           , # Prevalencia na populacao (%)
-  # p_recaida = 0.9           , # Probabilidade de novos eventos no futuro
-  # p_refratario = 0.1        , # Probabilidade de curso refratario
-
-  # efeito_tto = 0.1          , # Efeito do tratamento
-  # p_doenca_medicado = p_recaida * efeito_tto ,# Probabilidade de recaída após medicação
-  # p_cura = 0.001            , # Probabilidade de reversão para estágio assintomático
-  # custo_medicacao = 6000    ,
-  # custo_internacao = 100000 ,
-  # dr = 0.05                 , # Discount rate
-  # qaly_doenca = 0.5         , # QALY para um ano no estado sintomático
-
-
-  # p_internacao = 0.30,
-  # p_desinternacao = 0.20)
 
 # ------------------------------------------------------------------------
 # Transicoes
@@ -45,16 +29,16 @@ mat_base <- define_transition(
   state_names = c("geral", "pre", "symp", "death"),
 
   C,     prevalencia, 0 ,               p_death_all,
-  0,     C,           p_disease_base,   p_death_all, 
-  0,    p_cured,        C           ,   p_death_symp,
+  0,     C,           p_disease_base,   p_death_all,
+  0,    p_cured_base,        C           ,   p_death_symp,
   0,    0,             0,                1)
 
 mat_med <- define_transition(
   state_names = c("geral", "pre", "symp", "death"),
 
   C,     prevalencia, 0 ,               p_death_all,
-  0,     C,           p_disease_med,   p_death_all, 
-  0,    p_cured,        C           ,   p_death_symp,
+  0,     C,           p_disease_med,   p_death_all,
+  0,    p_cured_med,        C           ,   p_death_symp,
   0,    0,             0,                1)
 
 
@@ -72,7 +56,7 @@ mat_med <- define_transition(
 
 
 state_geral <- define_state(
-  cost_treat = 0
+  cost_treat = 0,
   cost_hospit = 0, # good health => no hospital expenses
   cost_total = 0,
   qaly = 1)
@@ -103,6 +87,7 @@ state_death <- define_state(
 strat_base <- define_strategy(
   transition = mat_base,
 
+  geral = state_geral,
   pre = state_pre,
   symp = state_symp,
   death = state_death)
@@ -110,6 +95,7 @@ strat_base <- define_strategy(
 strat_med <- define_strategy(
   transition = mat_med,
 
+  geral = state_geral,
   pre = state_pre,
   symp = state_symp,
   death = state_death)
@@ -133,3 +119,19 @@ res_mod <- run_model(
 res_mod
 
 plot(res_mod)
+
+
+# ------------------------------------------------------------------------
+# Plot
+# ------------------------------------------------------------------------
+
+plot(res_mod) +
+  xlab("Tempo (anos)") +
+  ylab("Nº de indivíduos") +
+  theme_light() +
+  scale_color_brewer(
+    name = "State",
+    palette = "Set1"
+  )
+
+
